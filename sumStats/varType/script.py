e555777f59859ea11e2ac6a818be81cf6c82dad6
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-07 17:15:20 -0400

    Cat.codes #12

--- sumStats/varType/script.py
+++ sumStats/varType/script.py
@@ -6,14 +6,17 @@ import pandas as pd
 # Import dataset as a Pandas Dataframe
 clothes = pd.read_csv('clothing.csv', index_col=0)
 
-# View the first five rows of the dataframe
+# Check the first five rows of the `clothes` dataframe with `.head()`.
 print(clothes.head())
-
-# Print the unique values of the `Rating` column
-print(clothes['Rating'].unique())
 
 # Change the data type of `Rating` to category
 clothes['Rating'] = pd.Categorical(clothes['Rating'], ['very unsatisfied', 'unsatisfied', 'neutral', 'satisfied', 'very satisfied'], ordered=True)
 
-# Recheck the values of `Rating` with .unique()
+# Check the order of the Rating column
 print(clothes['Rating'].unique())
+
+# Create rating_codes by encoding the `Rating` variable with .cat.codes 
+clothes['rating_codes'] = clothes['Rating'].cat.codes
+
+# Print the first five rows of the dataframe
+print(clothes.head())
