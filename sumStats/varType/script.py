6a517ae40c4af3566c6558fa17784ec5c91880b1
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-07 13:30:51 -0400

    Using .unique()to inspect categorical variables #4

--- sumStats/varType/script.py
+++ sumStats/varType/script.py
@@ -7,9 +7,11 @@ import pandas as pd
 movies = pd.read_csv("movie_show.csv", index_col=0)
 
 # View the first five rows of the dataframe
-print(movies.head(5))
+print(movies.head())
 
-# Set the correct value for rating_variable_type
-rating_variable_type = 'categorical'
-print(rating_variable_type)
+# Print the unique values in the country column
+print(movies.country.unique())
 
+# Set the correct value for country_variable_type
+country_variable_type = 'nominal'
+print(country_variable_type)
