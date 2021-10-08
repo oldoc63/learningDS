81f81550bfd0eb88812fd576662e4ddb81bfe049
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-08 09:33:46 -0400

    Median and Mean of Top 100 writers #25

--- median/script.py
+++ median/script.py
@@ -1,14 +1,29 @@
 # Import packages
+import codecademylib
 import numpy as np
 import pandas as pd
 
-# Read in author data
+# Import matplotlib pyplot
+from matplotlib import pyplot as plt
+
+# Read in transactions data
 greatest_books = pd.read_csv("top-hundred-books.csv")
 
-# Save author ages to author_ages
+# Save transaction times to a separate numpy array
 author_ages = greatest_books['Ages']
 
-# Use numpy to calculate the median age of the top 100 authors
-median_age = greatest_books['Ages'].median()
+# Use numpy to calculate the average age of the top 100 authors
+average_age = np.average(author_ages)
 
-print("The median age of the 100 greatest authors, according to a survey by Le Monde is: " + str(median_age))
+median_age = np.median(author_ages)
+
+# Plot the figure
+plt.hist(author_ages, range=(10, 80), bins=14,  edgecolor='black')
+plt.title("Age of Top 100 Novel Authors at Publication")
+plt.xlabel("Publication Age")
+plt.ylabel("Count")
+plt.axvline(average_age, color='r', linestyle='solid', linewidth=2, label="Mean")
+plt.axvline(median_age, color='y', linestyle='solid', linewidth=2, label="Median")
+plt.legend()
+
+plt.show()
