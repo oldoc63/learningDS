40c8920619f783a9c16ac52391bc94ef1405c4fb
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-08 09:20:54 -0400

    Calculating median with numpy #23

--- median/script.py
+++ median/script.py
@@ -1,29 +1,14 @@
 # Import packages
-import codecademylib
 import numpy as np
 import pandas as pd
 
-# Import matplotlib pyplot
-from matplotlib import pyplot as plt
-
-# Read in transactions data
+# Read in author data
 greatest_books = pd.read_csv("top-hundred-books.csv")
 
-# Save transaction times to a separate numpy array
+# Save author ages to author_ages
 author_ages = greatest_books['Ages']
 
-# Use numpy to calculate the average age of the top 100 authors
-average_age = np.average(author_ages)
+# Use numpy to calculate the median age of the top 100 authors
+median_age = greatest_books['Ages'].median()
 
-
-print("The average age of the 100 greatest authors, according to Le Monde is: " + str(average_age))
-
-# Plot the figure
-plt.hist(author_ages, range=(10, 80), bins=14,  edgecolor='black')
-plt.title("Age of Top 100 Novel Authors at Publication")
-plt.xlabel("Publication Age")
-plt.ylabel("Count")
-plt.axvline(average_age, color='r', linestyle='solid', linewidth=2, label="Mean")
-plt.legend()
-
-plt.show()
+print("The median age of the 100 greatest authors, according to a survey by Le Monde is: " + str(median_age))
