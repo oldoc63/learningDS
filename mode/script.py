58b1d66c39fc7f6d00bce7495fc1813c8fe19a97
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-08 14:46:08 -0400

    Mode, Median, Mean in a Histogram measuring central tendency #31

--- mode/script.py
+++ mode/script.py
@@ -1,17 +1,31 @@
 # Import packages
+import codecademylib
 import numpy as np
 import pandas as pd
 from scipy import stats
 
-# Read in author data
+# Import matplotlib pyplot
+from matplotlib import pyplot as plt
+
+# Read in transactions data
 greatest_books = pd.read_csv("top-hundred-books.csv")
 
-# Save author ages to author_ages
+# Save transaction times to a separate numpy array
 author_ages = greatest_books['Ages']
 
-# Use numpy to calculate the mode age of the top 100 authors
-mode_age = stats.mode(author_ages)
+# Calculate the average and median value of the author_ages array
+average_age = np.average(author_ages)
+median_age = np.median(author_ages)
+mode_age = 38
 
-#print(mode_age)
+# Plot the figure
+plt.hist(author_ages, range=(10, 80), bins=14,  edgecolor='black')
+plt.title("Author Ages at Publication")
+plt.xlabel("Publication Age")
+plt.ylabel("Count")
+plt.axvline(average_age, color='r', linestyle='solid', linewidth=3, label="Mean")
+plt.axvline(median_age, color='y', linestyle='dotted', linewidth=3, label="Median")
+plt.axvline(mode_age, color='orange', linestyle='dashed', linewidth=3, label="Mode")
+plt.legend()
 
-print("The mode age and its frequency of authors from Le Monde's 100 greatest books is: " + str(mode_age[0][0]) + " and " + str(mode_age[1][0]))
+plt.show()
