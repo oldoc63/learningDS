0444d658b58f29adb1fe3ac188811a3116225510
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-08 13:56:07 -0400

    Mode using scipy import stats #29

--- mode/script.py
+++ mode/script.py
@@ -1,15 +1,17 @@
+# Import packages
 import numpy as np
+import pandas as pd
+from scipy import stats
 
-# Set first_ten_authors equal to their ages
-first_ten_authors = np.array([29, 49, 42, 43, 32, 38, 37, 41, 27, 27])
+# Read in author data
+greatest_books = pd.read_csv("top-hundred-books.csv")
 
-# Save the mode value to mode_age
-mode_age = 27
+# Save author ages to author_ages
+author_ages = greatest_books['Ages']
 
-# Save the count of authors with the mode age
-mode_count = 2
+# Use numpy to calculate the mode age of the top 100 authors
+mode_age = stats.mode(author_ages)
 
-# Print the sorted array and median value
-print("The ages of the first ten authors is: " + str(first_ten_authors))
-print("The mode of the first ten authors is: " + str(mode_age))
-print("The number of authors who were " + str(mode_age) + " when their book was published is " + str(mode_count))
+#print(mode_age)
+
+print("The mode age and its frequency of authors from Le Monde's 100 greatest books is: " + str(mode_age[0][0]) + " and " + str(mode_age[1][0]))
