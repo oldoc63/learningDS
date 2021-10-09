2cd3bf52a39bd325e9094daafe39ffde79d1cb85
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-09 11:36:23 -0400

    Each of the differences squared using ** operator #47

--- variance/script.py
+++ variance/script.py
@@ -3,18 +3,16 @@ import numpy as np
 grades = [88, 82, 85, 84, 90]
 mean = np.mean(grades)
 
-difference_one = 88 - mean
-difference_two = 82 - mean
-difference_three = 85 - mean
-difference_four = 84 - mean
-difference_five = 90 - mean
+#When calculating these variables, square the difference.
+difference_one = (88 - mean) ** 2
+difference_two = (82 - mean) ** 2
+difference_three = (85 - mean) ** 2
+difference_four = (84 - mean) ** 2
+difference_five = (90 - mean) ** 2
 
-#Part 1: Sum the differences
 difference_sum = difference_one + difference_two + difference_three + difference_four + difference_five
 
-#Part 2: Average the differences
-average_difference = difference_sum / 5
+variance = difference_sum / 5
 
-#IGNORE CODE BELOW HERE
-print("The sum of the differences is " + str(format(difference_sum, ".16f")))
-print("The average difference is " + str(format(average_difference, ".16f")))
+print("The sum of the squared differences is " + str(difference_sum))
+print("The variance is " + str(variance))
