7813c27d6ae2908f52e03bbccc974c72ab6ea436
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-09 09:54:10 -0400

    Solution #45

--- variance/script.py
+++ variance/script.py
@@ -10,11 +10,11 @@ difference_four = 84 - mean
 difference_five = 90 - mean
 
 #Part 1: Sum the differences
-difference_sum = 0
+difference_sum = difference_one + difference_two + difference_three + difference_four + difference_five
 
 #Part 2: Average the differences
-average_difference = 0
+average_difference = difference_sum / 5
 
 #IGNORE CODE BELOW HERE
-print("The sum of the differences is " + str(format(difference_sum, "f")))
-print("The average difference is " + str(format(average_difference, "f")))
+print("The sum of the differences is " + str(format(difference_sum, ".16f")))
+print("The average difference is " + str(format(average_difference, ".16f")))
