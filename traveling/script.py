90f7d9e3a71b4bebf5762fdb942da20a34bbd93b
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-14 11:44:26 -0400

    Travel and Blooming #89

---
+++ traveling/script.py
@@ -0,0 +1,15 @@
+# import codecademylib3
+import codecademylib3
+import numpy as np
+from matplotlib import pyplot as plt
+
+# load in data
+in_bloom = np.loadtxt(open("in-bloom.csv"), delimiter=",")
+flights = np.loadtxt(open("flights.csv"), delimiter=",")
+
+# Plot the histograms
+
+
+
+
+plt.show()
