4ccfb180531438a00af8109631eac9968f4ea83a
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-15 15:47:58 -0400

    Second quartile odd an even dataset #117

--- quartiles/script.py
+++ quartiles/script.py
@@ -1,10 +1,19 @@
-import codecademylib3_seaborn
-from song_data import songs
-import matplotlib.pyplot as plt
+dataset_one = [50, 10, 4, -3, 4, -20, 2]
+#Sorted dataset_one: [-20, -3, 2, 4, 4, 10, 50]
 
-plt.hist(songs, bins = 200)
-plt.axvline(x=364, label="Chicago", c = 'r')
-plt.xlabel("Song Length (Seconds)")
-plt.ylabel("Count")
-plt.legend()
-plt.show()
+dataset_two = [24, 20, 1, 45, -15, 40]
+#Sorted dataset_two: [-15, 1, 20, 24, 40, 45]
+
+#Define the second quartile of both datasets here:
+dataset_one_q2 = 4
+dataset_two_q2 = 22
+
+#Ignore the code below here:
+try:
+  print("The second quartile of dataset one is " + str(dataset_one_q2))
+except NameError:
+  print("You haven't defined dataset_one_q2")
+try:
+  print("The second quartile of dataset two is " + str(dataset_two_q2))
+except NameError:
+  print("You haven't defined dataset_two_q2")
