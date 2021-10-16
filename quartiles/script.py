e9a2e01ea7cc94f88f5abf327c8dc61ddefcc62c
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-16 10:42:25 -0400

    Is my 3 min fav song above or below de 32th percentile? #131

--- quartiles/script.py
+++ quartiles/script.py
@@ -1,18 +1,12 @@
 from song_data import songs
 import numpy as np
 
-# Define quartiles, deciles, and tenth here:
-quartiles = np.quantile(songs, [0.25, 0.5, 0.75])
-deciles = np.quantile(songs, [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-
-tenth= 3
+# Define percentile and answer here:
+percentile = np.quantile(songs, 0.32)
+answer = "below"
 #Ignore the code below here:
 try:
-  print("The quartiles are " + str(quartiles) + "\n")
+  print("Your percentile is " + str(percentile) + "\n")
 except NameError:
-  print("You haven't defined quartiles.\n")
-try:
-  print("The deciles are " + str(deciles) + "\n")
-except NameError:
-  print("You haven't defined deciles.\n")
+  print("You haven't defined percentile")
 
