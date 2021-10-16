605726c0fa0b775e161ca9d411101000c585a1f4
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-16 10:17:00 -0400

    Groups of equal size #129

--- quartiles/script.py
+++ quartiles/script.py
@@ -1,12 +1,18 @@
 from song_data import songs
 import numpy as np
 
-# Define twenty_third_percentile here:
-twenty_third_percentile = np.quantile(songs, 0.23)
+# Define quartiles, deciles, and tenth here:
+quartiles = np.quantile(songs, [0.25, 0.5, 0.75])
+deciles = np.quantile(songs, [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
+tenth= 3
 #Ignore the code below here:
 try:
-  print("The value that splits 23% of the data is " + str(twenty_third_percentile) + "\n")
+  print("The quartiles are " + str(quartiles) + "\n")
 except NameError:
-  print("You haven't defined twenty_third_percentile.")
+  print("You haven't defined quartiles.\n")
+try:
+  print("The deciles are " + str(deciles) + "\n")
+except NameError:
+  print("You haven't defined deciles.\n")
 
