5fc32e17649e7c7586ed6a367c85a4502aa210c9
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-18 18:00:16 -0400

    Whiskers extend to the point closest to 1.5 times the IQR in the direction of the median #157

--- boxplots/script.py
+++ boxplots/script.py
@@ -1,16 +1,32 @@
 import numpy as np
 from data import dataset
 
-# Define quartile_one and quartile_three here:
-quartile_one = np.quantile(dataset, 0.25)
+quartile_one = np.quantile(dataset, 0.25) 
 quartile_three = np.quantile(dataset, 0.75)
+
+# Define your variables here:
+iqr =  quartile_three - quartile_one
+distance = 1.5 * iqr
+left_whisker = quartile_one - distance
+right_whisker = quartile_three + distance
 
 #Ignore the code below here
 try:
-  print("The first quartile of the dataset is " + str(quartile_one) + ".")
+  print("The interquartile range of the dataset is " + str(iqr) + ".")
 except NameError:
-  print("You haven't defined quartile_one.")
+  print("You haven't defined iqr.")
+  
 try:
-  print("The third quartile of the dataset is " + str(quartile_three) + ".")
+  print("Each whisker should be " + str(distance) + " units away from the edges of the box.")
 except NameError:
-  print("You haven't defined quartile_three.")
+  print("You haven't defined distance.")
+  
+try:
+  print("The left whisker should extend to " + str(left_whisker) + " .")
+except NameError:
+  print("You haven't defined left_whisker.")
+  
+try:
+  print("The right whisker should extend to " + str(right_whisker) + " .")
+except NameError:
+  print("You haven't defined right_whisker.")
