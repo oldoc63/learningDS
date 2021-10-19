db74a4065c90ca13c3709f4fe59f3665d2d29b99
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-19 12:26:08 -0400

    Separate and make one plot for the state of Alabama #171

--- healthcare/script.py
+++ healthcare/script.py
@@ -4,8 +4,14 @@ from matplotlib import pyplot as plt
 
 healthcare = pd.read_csv("healthcare.csv")
 
-print(healthcare.head())
-print(healthcare["DRG Definition"].unique())
+#print(healthcare.head())
+#print(healthcare["DRG Definition"].unique())
 
 chest_pain = healthcare[healthcare['DRG Definition'] == '313 - CHEST PAIN']
+alabama_chest_pain = chest_pain[chest_pain['Provider State'] == "AL"]
+costs = alabama_chest_pain[' Average Covered Charges '].values
+plt.boxplot(costs)
+plt.show()
+
+
 
