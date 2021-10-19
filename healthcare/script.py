8be42036086042c5b3c12921131c4d77c52cc9c8
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-19 13:05:54 -0400

    Chest Pain Average Cost in USA by State #173

--- healthcare/script.py
+++ healthcare/script.py
@@ -8,9 +8,17 @@ healthcare = pd.read_csv("healthcare.csv")
 #print(healthcare["DRG Definition"].unique())
 
 chest_pain = healthcare[healthcare['DRG Definition'] == '313 - CHEST PAIN']
-alabama_chest_pain = chest_pain[chest_pain['Provider State'] == "AL"]
-costs = alabama_chest_pain[' Average Covered Charges '].values
-plt.boxplot(costs)
+#alabama_chest_pain = chest_pain[chest_pain['Provider State'] == "AL"]
+#costs = alabama_chest_pain[' Average Covered Charges '].values
+
+states = chest_pain["Provider State"].unique()
+
+datasets = []
+for state in states:
+  datasets.append(chest_pain[chest_pain['Provider State'] == state][' Average Covered Charges '].values)
+
+plt.figure(figsize=(20,6))
+plt.boxplot(datasets, labels = states)
 plt.show()
 
 
