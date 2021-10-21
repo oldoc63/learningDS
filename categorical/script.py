6c69bc81492e291e265e8a520b6dd7eb9d7a41bb
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-21 08:55:01 -0400

    Categorical and cat.codes #193

--- categorical/script.py
+++ categorical/script.py
@@ -1,12 +1,24 @@
 import pandas as pd
+import numpy as np
 
-# Read NYC Trees Data
+# Read NYC trees data
 nyc_trees = pd.read_csv("./nyc_tree_census.csv")
 
-# Get tree counts by neighborhood
-tree_counts = nyc_trees.neighborhood.value_counts()
-print(tree_counts)
+# Find the unique values in the column health
+tree_health_statuses = nyc_trees.health.unique()
+print(tree_health_statuses)
 
-# Get neighborhoods with most trees
-greenest_neighborhood = tree_counts.index[0]
-print(greenest_neighborhood)
+#Create a list named health_categories which lists the categories from worst to best. Exclude NaN.
+health_categories = ['Poor', 'Fair', 'Good']
+
+#convert health to 'category'
+nyc_trees['health'] = pd.Categorical(nyc_trees['health'], health_categories, ordered=True)
+
+#calculate the median value of health
+median_index = np.median(nyc_trees['health'].cat.codes)
+print(median_index)
+median_health_status = health_categories[int(median_index)]
+print(median_health_status)
+
+
+
