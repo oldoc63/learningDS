9fd9026eaae5a1f0605e66e5413b8026c5a393a6
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-27 11:58:10 -0400

    Summarizing a nominal var with frequencies and proportions #209

--- summAutoEval/script.py
+++ summAutoEval/script.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 car_eval = pd.read_csv('car_eval_dataset.csv')
 print(car_eval.head())
+
+manufacturer_country_counts = car_eval.manufacturer_country.value_counts()
+print(manufacturer_country_counts)
+
+fourth_manufacturer_country = manufacturer_country_counts.index[3]
+print(fourth_manufacturer_country)
+
+manufacturer_country_proportions = car_eval.manufacturer_country.value_counts(normalize=True)
+print(manufacturer_country_proportions.head())
