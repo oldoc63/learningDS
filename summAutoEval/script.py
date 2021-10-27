4c021de75b27832b965bc4b70a008f1107f6ff1f
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-27 12:58:06 -0400

    Summarizin an ordinal category #211

--- summAutoEval/script.py
+++ summAutoEval/script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 car_eval = pd.read_csv('car_eval_dataset.csv')
 print(car_eval.head())
@@ -11,3 +12,15 @@ print(fourth_manufacturer_country)
 
 manufacturer_country_proportions = car_eval.manufacturer_country.value_counts(normalize=True)
 print(manufacturer_country_proportions.head())
+
+print(car_eval.buying_cost.unique())
+
+buying_cost_categories = ['low', 'med', 'high', 'vhigh']
+
+car_eval['buying_cost'] = pd.Categorical(car_eval['buying_cost'], buying_cost_categories, ordered=True)
+
+buying_cost_median = np.median(car_eval['buying_cost'].cat.codes)
+
+print(buying_cost_median)
+
+
