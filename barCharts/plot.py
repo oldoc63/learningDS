c5e5fd9b97a24cec72f6335ea39503be30cfc64f
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-29 08:42:24 -0400

    Ordering Bar Charts #221

--- barCharts/plot.py
+++ barCharts/plot.py
@@ -3,12 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("games.csv")
+df = pd.read_csv("school_data.csv")
 print(df.head())
 
-sns.countplot(df["victory_status"])
+value_order = ["NOT ENOUGH DATA", "VERY WEAK", "WEAK", "NEUTRAL", "STRONG", "VERY STRONG"]
 
+type_of_data = "ordinal"
+
+# plot using .countplot() method here
+sns.countplot(df['Supportive Environment'], order=value_order)
+
+plt.xticks(rotation=30)
+
+# show your plot here
 plt.show()
-
-
-
