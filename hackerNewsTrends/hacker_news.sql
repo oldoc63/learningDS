3c9c581499b89bbaf929ba31df98744937d22d1d
oldoc63 <leopoldo.olmos@gmail.com>
2022-06-25 18:06:10 -0400

    First, find the offending users #255

--- hackerNewsTrends/hacker_news.sql
+++ hackerNewsTrends/hacker_news.sql
@@ -13,3 +13,7 @@ HAVING SUM(score) > 200
 ORDER BY 2 DESC;
 
 SELECT (517 + 309 + 304 + 282) / 6366.0;
+
+SELECT user, url
+  FROM hacker_news
+  WHERE url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
