14fda958a59cba8482b94a79c0e2f4f07bc965c9
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-04 09:01:57 -0400

    table_name.colum_name syntax #272

--- multipleTables/multab.sql
+++ multipleTables/multab.sql
@@ -3,3 +3,9 @@ SELECT *
 FROM orders
 JOIN customers
 ON orders.customer_id = customers-customer_id;
+
+-- table_name.column_name syntax
+SELECT orders.order_id, customers.customer_name
+FROM orders
+JOIN customers
+ON orders.customer_id = customers.customer_id;
