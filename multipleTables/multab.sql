1854cb812be13b037d53118d03c9e43d4783506f
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-09 20:16:25 -0400

    We are putting a whole first query inside the parentheses () #293

--- multipleTables/multab.sql
+++ multipleTables/multab.sql
@@ -94,3 +94,17 @@ SELECT customer_id,
 COUNT(subscription_id) AS 'subscriptions'
 FROM orders
 GROUP BY customer_id;
+
+-- Using the WITH statement
+WITH previous_query AS (
+  SELECT customer_id,
+    COUNT(subscription_id) AS 'subscriptions'
+  FROM orders
+  GROUP BY customer_id
+)
+SELECT customers.customer_name,
+    previous_query.subscriptions
+FROM previous_query
+JOIN customers
+  ON previous_query.customer_id 
+  = customers.customer_id;
