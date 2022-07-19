5e0953414d6b040cdb6ff6730f851c68166caedd
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-19 12:26:30 -0400

    Common string.methods #333

---
+++ stringMethods/script.py
@@ -0,0 +1,15 @@
+print('Hello World'.upper())
+
+print('Hello World'.lower())
+
+print('hello world'.title())
+
+print('Hello World'.split())
+
+print(' '.join(['Hello', 'World']))
+
+print('Hello world'.replace('H', 'J'))
+
+print('   Hello World   '.strip())
+
+print('{} {}'.format('Hello', 'World'))
