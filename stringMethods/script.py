aa4c36eaf06c65a544b8c1efbf99313ebfd0d1d0
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-21 07:50:40 -0400

    featuringStrip #343

--- stringMethods/script.py
+++ stringMethods/script.py
@@ -116,3 +116,7 @@ print(smooth_fifth_verse)
 winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
 winter_trees_full = '\n'.join(winter_trees_lines)
 print(winter_trees_full)
+
+#Python provides a great method for cleaning strings: .strip()
+featuring = "          rob thomas         "
+print(featuring.strip())
