718386947e039424607146c26fbce0d6854dd605
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-22 07:32:19 -0400

    Print highlighted_poems #347

--- stringMethods/script.py
+++ stringMethods/script.py
@@ -195,3 +195,7 @@ def poem_description(publishing_date, author, title, original_work):
 my_beard_description = poem_description(author = "Shel Silverstein", title = "My Beard", original_work = "Where the Sidewalk Ends", publishing_date = "1974")
 
 print(my_beard_description)
+
+#Preserve the Verse final task
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+print(highlighted_poems)
