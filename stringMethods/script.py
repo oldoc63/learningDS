a5ea4ecb880235c3d2592c726796705c52bd7dfc
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-21 21:54:14 -0400

    My Beard Description Fixed #346

--- stringMethods/script.py
+++ stringMethods/script.py
@@ -186,3 +186,12 @@ def favorite_song_statement(song, artist):
 
 print(favorite_song_statement('Smooth', 'Santana'))
 # => "My favorite song is Smooth by Santana."
+
+# Fix function poem_description by using keywords
+def poem_description(publishing_date, author, title, original_work):
+    poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date=publishing_date, author=author, title=title, original_work=original_work)
+    return poem_desc
+
+my_beard_description = poem_description(author = "Shel Silverstein", title = "My Beard", original_work = "Where the Sidewalk Ends", publishing_date = "1974")
+
+print(my_beard_description)
