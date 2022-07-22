9f3aec7c8ebe163ec4b36c2240ba49ec2ca6c659
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-22 07:40:21 -0400

    Split highlighted_poems at the commas #347

--- stringMethods/script.py
+++ stringMethods/script.py
@@ -199,3 +199,6 @@ print(my_beard_description)
 #Preserve the Verse final task
 highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
 print(highlighted_poems)
+
+#Split highlighted_poems at the commas
+highlighted_poems_list = highlighted_poems.split(',')
