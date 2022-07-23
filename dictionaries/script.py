ee5e1a9637c20c0586ec47bde9df52be27b36208
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-23 19:57:25 -0400

    TypeError in Python Dictionary #350

--- dictionaries/script.py
+++ dictionaries/script.py
@@ -1,2 +1,7 @@
 #Creating a dictionary
 menu = {"avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+
+#TypeError
+#powers = {[1, 2, 4, 8, 16]: 2, [1, 3, 9, 27, 81]: 3}
+#TypeError: unhashable type: 'list'
+
