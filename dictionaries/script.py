337a5af25a247a1001fa99555e8d2d7b080d2fa3
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-23 20:09:17 -0400

    Empty Dictionary #350

--- dictionaries/script.py
+++ dictionaries/script.py
@@ -5,3 +5,5 @@ menu = {"avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
 #powers = {[1, 2, 4, 8, 16]: 2, [1, 3, 9, 27, 81]: 3}
 #TypeError: unhashable type: 'list'
 
+#Empty Dictionary
+empty_dict = {}
