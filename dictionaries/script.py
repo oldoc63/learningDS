147155c2ded13cb6eacde21c58b115513fc989b3
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-24 08:06:13 -0400

    You can access the value in a dict by providing the key between brackets #352

--- dictionaries/script.py
+++ dictionaries/script.py
@@ -34,3 +34,7 @@ heights = [61, 70, 67, 64]
 students = {key:value for key, value in zip(names, heights)}
 
 print(students)
+
+#Get a key
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+print(zodiac_elements['water'])
