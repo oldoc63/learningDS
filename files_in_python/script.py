42a5351152d85211b181c55cb7107510f89a993b
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-24 16:47:04 -0400

    The .readlines() function read a text file line by line #354

--- files_in_python/script.py
+++ files_in_python/script.py
@@ -1,4 +1,9 @@
-#Open a file
+#Open a file and read the whole document
 with open('files_in_python/real_cool_document.txt') as cool_doc:
     cool_contents = cool_doc.read()
 print(cool_contents)
+
+#Open a file and read it line by line
+with open('keats_sonnet.txt') as keats_sonnet:
+    for line in keats_sonnet.readlines():
+        print(line)
