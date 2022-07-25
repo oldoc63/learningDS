51aa55a69dbf92a6fdd29e6483c492be6f8968dd
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-25 08:13:17 -0400

    Using DictReaderfrom csv Python library #360

--- files_in_python/script.py
+++ files_in_python/script.py
@@ -23,7 +23,7 @@ with open('files_in_python/generated_file.txt', 'a') as gen_file:
     gen_file.write("... and it still is")
 
 #Access files with .close()
-fun_cities_file = open(files_in_python/fun_cities.txt, 'a')
+fun_cities_file = open('files_in_python/fun_cities_file.txt', 'a')
 
 #We can now append a line to "fun_cities"
 fun_cities_file.write("Montréal")
@@ -32,3 +32,15 @@ fun_cities_file.write("Montréal")
 fun_cities_file.close()
 
 #.csv
+
+#Reading a .csv file
+
+import csv
+
+list_of_email_addresses = []
+with open('files_in_python/users.csv', newline='') as users_csv:
+    user_reader = csv.DictReader(users_csv)
+    for row in user_reader:
+        list_of_email_addresses.append(row['Email'])
+
+print(list_of_email_addresses)
