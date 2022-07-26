8b2e8f675f3d0bfd1b2d0f72ea8c5978cabad983
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-25 21:30:55 -0400

    Programmatically create CSV file #362

--- files_in_python/script.py
+++ files_in_python/script.py
@@ -47,7 +47,20 @@ print(list_of_email_addresses)
 #Reading different types of CSV files
 import csv
 
-with open('addresses.csv', newline='') as addresses_csv:
+with open('files_in_python/addresses.csv', newline='') as addresses_csv:
     address_reader = csv.DictReader(addresses_csv, delimiter=';')
     for row in address_reader:
         print(row['Address'])
+
+#Writing a CSV file
+big_list = [{'name': 'Fredrick Stein', 'userid': 6712359021, 'is_admin': False}, {'name': 'Wiltmore Denis', 'userid': 2525942, 'is_admin': False}, {'name': 'Greely Plonk', 'userid': 15890235, 'is_admin': False}, {'name': 'Dendris Stulo', 'userid': 572189563, 'is_admin': True}] 
+
+import csv
+
+with open('files_in_python/output.csv', 'w') as output_csv:
+    fields = ['name', 'userid', 'is_admin']
+    output_writer = csv.DictWriter(output_csv, fieldnames=fields)
+    output_writer.writeheader()
+    for item in big_list:
+        output_writer.writerow(item)
+
