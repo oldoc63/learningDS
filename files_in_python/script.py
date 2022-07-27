595855ae2edd1bbbbc99b79c50b677cb7b83baea
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-26 21:32:26 -0400

    Use the json.dump() method to write the file #364

--- files_in_python/script.py
+++ files_in_python/script.py
@@ -71,3 +71,16 @@ with open('files_in_python/purchase_14781239.json') as purchase_json:
     purchase_data = json.load(purchase_json)
 
 print(purchase_data['user'])
+
+#Writing a JSON File
+turn_to_json = {
+  'eventId': 674189,
+  'dateTime': '2015-02-12T09:23:17.511Z',
+  'chocolate': 'Semi-sweet Dark',
+  'isTomatoAFruit': True
+}
+
+import json
+
+with open('files_in_python/output.json', 'w') as json_file:
+    json.dump(turn_to_json, json_file)
