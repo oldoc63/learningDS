6a168ee58751b7502026fd4fb00d950c3b8003a2
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-28 07:58:04 -0400

    It is substring #365

--- lambda/script.py
+++ lambda/script.py
@@ -4,3 +4,11 @@ add_two = lambda my_input: my_input + 2
 print(add_two(3))
 print(add_two(100))
 print(add_two(-2))
+
+# is_substring
+is_substring = lambda my_string: my_string in "This is the master string"
+
+print(is_substring('I'))
+print(is_substring('am'))
+print(is_substring('the'))
+print(is_substring('master'))
