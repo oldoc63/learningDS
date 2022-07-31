403ed12c96e7bbafd1a015a6541f4bc168ae1e95
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-30 20:13:49 -0400

    Using lists to add data to a DataFrame #368

--- pandas/createDF.py
+++ pandas/createDF.py
@@ -6,4 +6,13 @@ df1 = pd.DataFrame({
     'age': [34, 28, 51]
 })
 
-print(df1)
+#print(df1)
+
+df2 = pd.DataFrame([
+    ['John Smith', '123 Main St.', 34],
+    ['Jane Doe', '456 Maple Ave.', 28],
+    ['Joe Schmo', '789 Broadway', 51]
+],
+columns=['name', 'address', 'age'])
+
+print(df2)
