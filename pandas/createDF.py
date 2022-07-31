68e71fdf7270a40bdb903394def5c53916e745c7
oldoc63 <leopoldo.olmos@gmail.com>
2022-07-30 20:01:24 -0400

    Create a DataFrame #368

---
+++ pandas/createDF.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df1 = pd.DataFrame({
+    'name': ['John Smith', 'Jane Doe', 'Joe Schmo'],
+    'address': ['123 Main St.', '456 Maple Ave.', '789 Broadway'],
+    'age': [34, 28, 51]
+})
+
+print(df1)
