45918e0d163ab51d40cdaf627d75763ed658c585
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-06 20:43:20 -0400

    Return first and last letters of a string #381

--- pandas/modifyingDF.py
+++ pandas/modifyingDF.py
@@ -58,3 +58,7 @@ print(mylambda(5))
 # Converts a string into lowercase
 stringlambda = lambda x: x.lower()
 print(stringlambda("Oh Hi Mark!"))
+
+# Return first and last letters of a string
+mylambda = lambda s: s[0] + s[-1]
+print(mylambda('This is a string'))
