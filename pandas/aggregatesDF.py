593a684451940a9d73afd93d0d5b1382ad83832c
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-10 09:03:27 -0400

    Find the numbers of each category of tea #383

--- pandas/aggregatesDF.py
+++ pandas/aggregatesDF.py
@@ -21,13 +21,18 @@ print(pricey_shoes)
 
 print(type(pricey_shoes))
 
-#DataFrame teas
+# DataFrame teas
 teas = pd.DataFrame([
     [0, 'earl grey', 'black', 38, 3],
     [1, 'english breakfast', 'black', 41, 3],
     [2, 'irish breakfast', 'black', 37, 2.5],
     [3, 'jasmine', 'green', 23, 4.5],
     [4, 'matcha', 'green', 48, 5],
-    [5, 'camomile', 'green', 0, 3]
+    [5, 'camomile', 'herbal', 0, 3]
     ],
     columns=['id', 'tea', 'category', 'caffeine', 'price'])
+
+# Find the number of each category of tea
+teas_counts = teas.groupby('category').id.count().reset_index()
+
+print(teas_counts)
