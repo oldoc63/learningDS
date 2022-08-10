54953c60472e710232142cc977cd8bc0a33c57ab
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-10 08:48:38 -0400

    Create teas DataFrame #383

--- pandas/aggregatesDF.py
+++ pandas/aggregatesDF.py
@@ -15,9 +15,19 @@ num_colors = orders.shoe_color.nunique()
 print(num_colors)
 
 # Using .groupby to calculate aggregates
-
 pricey_shoes = orders.groupby('shoe_type').price.max()
 
 print(pricey_shoes)
 
 print(type(pricey_shoes))
+
+#DataFrame teas
+teas = pd.DataFrame([
+    [0, 'earl grey', 'black', 38, 3],
+    [1, 'english breakfast', 'black', 41, 3],
+    [2, 'irish breakfast', 'black', 37, 2.5],
+    [3, 'jasmine', 'green', 23, 4.5],
+    [4, 'matcha', 'green', 48, 5],
+    [5, 'camomile', 'green', 0, 3]
+    ],
+    columns=['id', 'tea', 'category', 'caffeine', 'price'])
