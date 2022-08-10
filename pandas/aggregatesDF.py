82d1c3d7dd71852f0a315fe380dd42876030870e
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-10 11:23:28 -0400

    Print pricey_shoes and pricey_shoes type #383

--- pandas/aggregatesDF.py
+++ pandas/aggregatesDF.py
@@ -44,3 +44,7 @@ print(teas_counts)
 
 # Change pricey_shoes into a DataFrame
 pricey_shoes = orders.groupby('shoe_type').price.max().reset_index()
+
+print(pricey_shoes)
+
+print(type(pricey_shoes))
