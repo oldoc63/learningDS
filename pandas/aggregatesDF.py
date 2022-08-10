73ad3c9d1dc8ab93ad9700602d0138a17984838f
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-09 23:03:25 -0400

    Using .groupby to calculate aggregates #383

--- pandas/aggregatesDF.py
+++ pandas/aggregatesDF.py
@@ -13,3 +13,11 @@ print(most_expensive)
 num_colors = orders.shoe_color.nunique()
 
 print(num_colors)
+
+# Using .groupby to calculate aggregates
+
+pricey_shoes = orders.groupby('shoe_type').price.max()
+
+print(pricey_shoes)
+
+print(type(pricey_shoes))
