a01ab2981c55c180b41eb54624e1c36bc2ac6135
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-09 20:16:06 -0400

    Revisiting orders DF #383

---
+++ pandas/aggregatesDF.py
@@ -0,0 +1,15 @@
+# Revisiting orders DataFrame
+
+import pandas as pd
+
+orders = pd.read_csv('learningDS/pandas/orders.csv')
+
+print(orders.head(10))
+
+most_expensive = orders.price.max()
+
+print(most_expensive)
+
+num_colors = orders.shoe_color.nunique()
+
+print(num_colors)
