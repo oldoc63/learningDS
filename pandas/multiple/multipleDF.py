a299e21658479c11a77201e3fc03dd59b4cfb739
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-11 09:03:53 -0400

    Efficiently store dataspreading related information across multiple tables #386

---
+++ pandas/multiple/multipleDF.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+orders = pd.read_csv('learningDS/pandas/multiple/orders.csv')
+
+products = pd.read_csv('learningDS/pandas/multiple/products.csv')
+
+customers = pd.read_csv('learningDS/pandas/multiple/customers.csv')
+
+print(orders)
+
+print(products)
+
+print(customers)
