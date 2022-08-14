1400b3a5960ce0dc7717b425b54cab3aa044005a
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-14 08:31:59 -0400

    Mismatch: there is no product_id 5 in products #387

---
+++ pandas/mismatch/mismatched.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+orders = pd.read_csv('learningDS/pandas/mismatch/orders.csv')
+print(orders)
+
+products = pd.read_csv('learningDS/pandas/mismatch/products.csv')
+print(products)
