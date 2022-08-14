7ef7489fe48b6ff8a17a45bc9df837c84a13869f
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-14 12:19:50 -0400

    Create company_a and company_b DataFrames #388

---
+++ pandas/outer/outer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+company_a = pd.DataFrame([
+    ['Sally Sparrow', 'sally.sparrow@gmail.com'],
+    ['Peter Grant', 'pgrant@yahoo.com'],
+    ['Leslie May', 'leslie_may@gmail.com']
+    ],
+    columns=['name','email']
+)
+
+company_b = pd.DataFrame([
+    {'name': 'Peter Grant','phone': '212-345-6789'},
+    {'name': 'Leslie May', 'phone': '626-987-6543'},
+    {'name': 'Aaron Burr', 'phone': '303-456-7891'}
+])
+
+print(company_a)
+
+print(company_b)
