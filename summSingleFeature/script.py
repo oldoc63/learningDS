4cf2e6dde6c16779a0f790adaf41ba4981a75fe8
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-18 19:08:40 -0400

    Display summary statistics for all columns #392

--- summSingleFeature/script.py
+++ summSingleFeature/script.py
@@ -6,3 +6,6 @@ print(rentals.head())
 
 rentals_subset = rentals[['rent', 'size_sqft', 'borough']]
 print(rentals_subset)
+
+# Display summary statistics for all columns
+print(rentals_subset.describe(include='all'))
