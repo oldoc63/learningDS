dde908e7a7dd854033cc728b2da64134bff27e2d
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-21 13:00:44 -0400

    Data Information: print(df.info()) #399

--- inspectCleanValidate/script.py
+++ inspectCleanValidate/script.py
@@ -1,5 +1,11 @@
+# Initial Data Inspection
+
 import pandas as pd
+
 heart = pd.read_csv('inspectCleanValidate/processed.cleveland.data.csv')
 print(heart.head())
 
 print(heart.num.value_counts())
+
+# Data Information
+print(heart.info())
