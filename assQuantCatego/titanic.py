dd0769ff2ab1d9c4287d43784e1b2a9eff717293
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-28 09:03:55 -0400

    Calculate the difference in median fare paid #401

--- assQuantCatego/titanic.py
+++ assQuantCatego/titanic.py
@@ -17,3 +17,10 @@ mean_fares_survived = np.mean(fares_survived)
 mean_diff = mean_fares_survived - mean_fares_died
 print('mean difference: ')
 print(mean_diff)
+
+# Median difference
+median_fares_died = np.median(fares_died)
+median_fares_survived = np.median(fares_survived)
+med_diff = median_fares_survived - median_fares_died
+print('median difference: ')
+print(med_diff)
