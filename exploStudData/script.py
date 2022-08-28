a84b973d1b695687876d8d15cde79010a77a7a71
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-28 16:47:13 -0400

    Calculate and print the mean value of math_grade with numpy #403

--- exploStudData/script.py
+++ exploStudData/script.py
@@ -9,3 +9,12 @@ students = pd.read_csv('exploStudData/students.csv')
 
 # Print first few rows of data
 print(students.head())
+
+# Print the summary statistics for all columns
+print(students.describe())
+
+# Summarize a typical student grade
+
+# Calculate and print the mean value of math_grade with numpy
+mean_math_grade = np.mean(students.math_grade)
+print(mean_math_grade)
