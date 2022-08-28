d1b2ae06b7d97764c218412a593c95bc0988a877
oldoc63 <leopoldo.olmos@gmail.com>
2022-08-28 16:23:34 -0400

    Print first few rows of the dataset students #403

--- exploStudData/script.py
+++ exploStudData/script.py
@@ -4,3 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Import data
+students = pd.read_csv('exploStudData/students.csv')
+
+# Print first few rows of data
+print(students.head())
