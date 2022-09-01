384a5a64adb49492b079ced4b865d3e4efe99704
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-01 14:24:00 -0400

    Plot the area of a rental against its monthly price #404

--- assTwoQuant/script.py
+++ assTwoQuant/script.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 housing = pd.read_csv('assTwoQuant/housing_sample.csv')
 
 # Print the first ten rows of data
 print(housing.head(10))
+
+# Plot the area of a rental against its monthly price
+plt.scatter(x=housing.price, y=housing.sqfeet) 
+plt.xlabel('Rental Price (USD)')
+plt.ylabel('Area (Square Feet')
+plt.show()
