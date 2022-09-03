5ed8752e1cd977f91186891d8c6591f6227d165a
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-03 10:34:43 -0400

    Investigate association between special and authority #409

--- assTwoCatego/script.py
+++ assTwoCatego/script.py
@@ -7,3 +7,7 @@ print(npi.head())
 # Contingency Tables: crosstab function from pandas
 influence_leader_freq = pd.crosstab(npi.influence, npi.leader)
 print(influence_leader_freq)
+
+# Investigate association between special and authority
+special_authority_freq = pd.crosstab(npi.special, npi.authority)
+print(special_authority_freq)
