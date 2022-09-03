5eabe8d1aa166133c3826b71ed0882e97affd6a1
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-03 09:41:25 -0400

    Contingency Tables: crosstab function from pandas #409

--- assTwoCatego/script.py
+++ assTwoCatego/script.py
@@ -3,3 +3,7 @@ import pandas as pd
 npi = pd.read_csv('assTwoCatego/npi_sample.csv')
 
 print(npi.head())
+
+# Contingency Tables: crosstab function from pandas
+influence_leader_freq = pd.crosstab(npi.influence, npi.leader)
+print(influence_leader_freq)
