38d607295aa465ecac8e3baa5ed4de1ee4746832
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-26 15:29:15 -0400

    Probability of observing 8 or fewer heads adding all up #424

--- probability/binom.py
+++ probability/binom.py
@@ -26,3 +26,15 @@ print('The probability of observing between 2 and 4 heads from 10 coin flips is'
 # calculating P(less than 3 heads) = P(0 heads) + P(1 head) +P(2 heads) for flipping a coin 10 times
 print('The probability of observing less than 3 heads is', 
 stats.binom.pmf(0, 10, 0.5) + stats.binom.pmf(1, 10, 0.5) + stats.binom.pmf(2, 10, 0.5))
+
+# probability of observing 8 or fewer heads
+print('The probability of observing 8 or fewer heads',
+stats.binom.pmf(0, n=10, p=0.5)
++ stats.binom.pmf(1, n=10, p=0.5)
++ stats.binom.pmf(2, n=10, p=0.5)
++ stats.binom.pmf(3, n=10, p=0.5)
++ stats.binom.pmf(4, n=10, p=0.5)
++ stats.binom.pmf(5, n=10, p=0.5)
++ stats.binom.pmf(6, n=10, p=0.5)
++ stats.binom.pmf(7, n=10, p=0.5)
++ stats.binom.pmf(8, n=10, p=0.5))
