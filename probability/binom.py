2f13ca27fa73d99642ac4ad89779a7546513031f
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-26 14:41:09 -0400

    The probability of observing between two and four heads from 10 coin flips #424

--- probability/binom.py
+++ probability/binom.py
@@ -18,3 +18,7 @@ print(prob_1)
 ## Question 2
 prob_2 = stats.binom.pmf(7, 20, 0.5)
 print(prob_2)
+
+# calculating P(2-4 heads) = P(2 heads) + P(3 heads) + p(4 heads) for flipping a coin 10 times
+print('The probability of observing between 2 and 4 heads from 10 coin flips is',
+ stats.binom.pmf(2, n=10, p=0.5) + stats.binom.pmf(3, n=10, p=0.5) + stats.binom.pmf(4, n=10, p=0.5))
