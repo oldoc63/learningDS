facfcca91a990f7c0a52990816f4ade9661b8786
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-25 22:11:37 -0400

    Probability of observing 3 heads in 10 fair coin flips #423

--- probability/binom.py
+++ probability/binom.py
@@ -1,4 +1,16 @@
 import scipy.stats as stats
 
 # st.binom.pmf(x, n, p)
-print(stats.binom.pmf(6, 10, 0.5))
+#print(stats.binom.pmf(6, 10, 0.5))
+
+# value of interest
+# change this
+x = 3
+
+# sample size
+# change this
+n = 10
+
+# calculate probability
+prob_1 = stats.binom.pmf(x, n, 0.5)
+print(prob_1)
