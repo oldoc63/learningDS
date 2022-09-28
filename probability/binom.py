f65490108974aa065ffbb47f86c79c9e74736b33
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-28 10:17:34 -0400

    Probability of mmore than five heads #425

--- probability/binom.py
+++ probability/binom.py
@@ -57,12 +57,16 @@ print('The probability of six or fewer heads is', stats.binom.cdf(6, 10, 0.5))
 # prob of observing between 4 and 8 heads
 print('The probability between 4 and 8 heads is', stats.binom.cdf(8, 10, 0.5) - stats.binom.cdf(3, 10, 0.5))
 
-# prob of observinb more than 6 heads
+# prob of observing more than 6 heads
 print('The probability of observing more than 6 heads is', 1 - stats.binom.cdf(6, 10, 0.5))
 
 # Checkpoint 1
 prob_1 = stats.binom.cdf(3, 10, 0.5)
-print('Probability of three or fewer heads', prob_1)
+print('Probability of three or fewer heads using cdf', prob_1)
 
 # compare to pmf code
-print(stats.binom.pmf(0, n=10, p=.5) + stats.binom.pmf(1, n=10, p=.5) + stats.binom.pmf(2, n=10, p=.5) + stats.binom.pmf(3, n=10, p=.5))
+print('Probability of three or fewer heads using pmf', stats.binom.pmf(0, n=10, p=.5) + stats.binom.pmf(1, n=10, p=.5) + stats.binom.pmf(2, n=10, p=.5) + stats.binom.pmf(3, n=10, p=.5))
+
+## Checkpoint 2
+prob_2 = 1 - stats.binom.cdf(5, 10, 0.5)
+print('Probability of five or more heads is', prob_2)
