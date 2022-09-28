78060fb588f144afc9372d8616f58b2e934f5a29
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-28 14:44:29 -0400

    Probability of finding a woman less than 175 cm tall #426

--- probability/norm.py
+++ probability/norm.py
@@ -3,3 +3,6 @@ import scipy.stats as stats
 # Probability of finding a woman less than 158 cm tall
 # stats.norm.cdf(x, loc, scale)
 print('Probability of a woman less than 158 cm tall is', stats.norm.cdf(158, 167.64, 8))
+
+# Probability of finding a woman less than 175 cm tall
+print('Probability of a woman less than 175 cm tall is', stats.norm.cdf(175, 167.64, 8))
