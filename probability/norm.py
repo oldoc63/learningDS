70885edef962394d7b6cb11247d8e5e7ffea24ad
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-29 17:26:05 -0400

    Probability of a woman taller than 172 cm #426

--- probability/norm.py
+++ probability/norm.py
@@ -10,3 +10,7 @@ print('Probability of a woman less than 175 cm tall is', stats.norm.cdf(175, 167
 # P(165<X<175) = P(X<175) - P(X<165)
 # stats.norm.cdf(x, loc, scale) - stats.norm.cdf(x, loc, scale)
 print('Probability of a woman between 165 and 175 cm is', stats.norm.cdf(175, 167.74, 8) - stats.norm.cdf(165, 167.74, 8))
+
+# P(X>172) = 1 - P(X<172)
+# 1 - stats.norm.cdf(x, loc, scale)
+print('Probability of a woman taller than 172 is', 1 - stats.norm.cdf(172, 167.74, 8))
