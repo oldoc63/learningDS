0a63c5398f23eec14cb962f4e647f1f1dd7acd18
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-29 17:05:35 -0400

    Probability of a woman between 165 and 175 cm #426

--- probability/norm.py
+++ probability/norm.py
@@ -6,3 +6,7 @@ print('Probability of a woman less than 158 cm tall is', stats.norm.cdf(158, 167
 
 # Probability of finding a woman less than 175 cm tall
 print('Probability of a woman less than 175 cm tall is', stats.norm.cdf(175, 167.64, 8))
+
+# P(165<X<175) = P(X<175) - P(X<165)
+# stats.norm.cdf(x, loc, scale) - stats.norm.cdf(x, loc, scale)
+print('Probability of a woman between 165 and 175 cm is', stats.norm.cdf(175, 167.74, 8) - stats.norm.cdf(165, 167.74, 8))
