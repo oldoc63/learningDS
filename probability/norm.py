40df5890623930c0d8ac8ed60ae15befaf0a0e07
oldoc63 <leopoldo.olmos@gmail.com>
2022-09-29 21:14:04 -0400

    Time in Galapagos #426

--- probability/norm.py
+++ probability/norm.py
@@ -14,3 +14,11 @@ print('Probability of a woman between 165 and 175 cm is', stats.norm.cdf(175, 16
 # P(X>172) = 1 - P(X<172)
 # 1 - stats.norm.cdf(x, loc, scale)
 print('Probability of a woman taller than 172 is', 1 - stats.norm.cdf(172, 167.74, 8))
+
+# Probability of temperature between 18 and 25 degrees Celsius in Galapagos
+temp_prob_1 = stats.norm.cdf(25, 20, 3) - stats.norm.cdf(18, 20, 3)
+print('Probability of temperature between 18 and 25 degrees Celsius in Galapagos is', temp_prob_1)
+
+# Probability of temperature greater than 24 degrees Celsius in Galapagos
+temp_prob_2 = 1 - stats.norm.cdf(24, 20, 3)
+print('Probability of temperature greater than 24 degrees Celsius in Galapagos is', temp_prob_2)
