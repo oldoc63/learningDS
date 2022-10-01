95d8083b407aa26b17465a56aa2de3e91e189837
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-01 10:30:06 -0400

    Probability of 15 calls Poisson(15) #427

--- probability/poisson.py
+++ probability/poisson.py
@@ -5,3 +5,6 @@ print('Probability of raining 6 days Poisson(10) is', stats.poisson.pmf(6, 10))
 
 # Probability of raining 12-14 times Poisson(10)
 print('Probability of raining 12-14 times Poisson(10) is', stats.poisson.pmf(12, 10) + stats.poisson.pmf(13, 10) + stats.poisson.pmf(14, 10))
+
+# Probability of 15 calls Poisson(15)
+print('Probability of 15 calls Poisson(15) is', stats.poisson.pmf(15, 15))
