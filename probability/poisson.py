32d649d913cfe64c259767db044a9f2b2fb660ae
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-01 10:13:26 -0400

    Probability of raining 12-14 times Poisson(10) #427

--- probability/poisson.py
+++ probability/poisson.py
@@ -1,4 +1,7 @@
 import scipy.stats as stats
 
-# expected value=10, probability of observing a6
-stats.poisson.pmf(6, 10)
+# expected value=10, probability of observing 6
+print('Probability of raining 6 days Poisson(10) is', stats.poisson.pmf(6, 10))
+
+# Probability of raining 12-14 times Poisson(10)
+print('Probability of raining 12-14 times Poisson(10) is', stats.poisson.pmf(12, 10) + stats.poisson.pmf(13, 10) + stats.poisson.pmf(14, 10))
