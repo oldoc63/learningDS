1f5dee58ababd69fdd5a3a88c61b62c080007421
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-02 18:59:33 -0400

    Exercises #428

--- probability/poisson.py
+++ probability/poisson.py
@@ -57,3 +57,30 @@ print('The range of a random Poisson(4)', min(rand_vars), max(rand_vars))
 # Increase lambda to 10 to see the increase in range
 rand_vars = stats.poisson.rvs(10, size=1000)
 print('The range of a random Poisson(10)', min(rand_vars), max(rand_vars))
+
+# Exercises:
+
+## For checkpoints 1 and 2
+# 5000 draws, lambda = 7
+rand_vars_7 = stats.poisson.rvs(7, size = 5000)
+
+## Checkpoint 1
+# print variance of rand_vars_7
+print(np.var(rand_vars_7))
+
+## Checkpoint 2
+# print minimum and maximum of rand_vars_7
+print(min(rand_vars_7), max(rand_vars_7))
+
+
+## For checkpoints 3 and 4
+# 5000 draws, lambda = 17
+rand_vars_17 = stats.poisson.rvs(17, size = 5000)
+
+## Checkpoint 3
+# print variance of rand_vars_17
+print(np.var(rand_vars_17))
+
+# Checkpoint 4
+# print minimum and maximum of rand_vars_17
+print(min(rand_vars_17), max(rand_vars_17))
