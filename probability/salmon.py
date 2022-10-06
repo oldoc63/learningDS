e2c8be2c8ed97b8cffa0a391dbddac5cec0e9226
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-06 15:38:21 -0400

    Sampling distribution of the mean #434

--- probability/salmon.py
+++ probability/salmon.py
@@ -16,7 +16,7 @@ plt.xlabel("Weight (lbs)")
 plt.show()
 plt.clf() # close this plot
 
-samp_size = 30
+samp_size = 50
 
 # Generate our random sample below
 sample = np.random.choice(np.array(population), samp_size, replace=False)
@@ -29,4 +29,20 @@ sns.histplot(sample, stat='density')
 plt.axvline(sample_mean,color='r',linestyle='dashed')
 plt.title(F"Sample Mean: {sample_mean}")
 plt.xlabel("Weight (lbs)")
+plt.show()
+
+# loop 500 times to get 500 random sample means
+sample_means = []
+
+for i in range(500):
+    # take a sample from the data:
+    samp = np.random.choice(population, samp_size, replace=False)
+    # Calculate the mean of this sample:
+    this_sample_mean = np.mean(samp)
+    # append this sample mean to a list of sample means
+    sample_means.append(this_sample_mean)
+
+# plot all the sample means to show the sampling distribution
+sns.histplot(sample_means, stat='density')
+plt.title('Sampling distribution of the Mean')
 plt.show()
