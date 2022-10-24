d095e6b7a07fc0fab1a4e483252cf08db72ee9d0
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-24 14:47:07 -0400

    Running sum #436

--- mathAdventures/A-1-Practice/script_2.py
+++ mathAdventures/A-1-Practice/script_2.py
@@ -8,3 +8,11 @@ def mySum(num):
     return running_sum
 
 print(mySum(100))
+
+def mySum2(num):
+    running_sum = 0
+    for i in range(num + 1):
+        running_sum += i**2 + 1
+    return running_sum
+
+print(mySum2(20))
