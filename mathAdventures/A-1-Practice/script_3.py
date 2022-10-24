4efd7ca9010405dcabcda39539126dad3aa08be1
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-24 19:08:56 -0400

    Wow, you're old! #437

--- mathAdventures/A-1-Practice/script_3.py
+++ mathAdventures/A-1-Practice/script_3.py
@@ -2,3 +2,15 @@
 y = 7
 if y > 5:
     print('yes!')
+
+age = 50
+if age < 10:
+    print('What school do you go to?')
+elif 11 < age < 20:
+    print("You're cool!")
+elif 20 <= age < 30:
+    print("What job do you have?")
+elif 30 <= age < 40:
+    print('Are you married?')
+else:
+    print("Wow, you're old!")
