60d9825c0930bb01a533650fa4110c1981637de3
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-24 14:54:29 -0400

    Average made from two built in functions #436

--- mathAdventures/A-1-Practice/script_2.py
+++ mathAdventures/A-1-Practice/script_2.py
@@ -16,3 +16,8 @@ def mySum2(num):
     return running_sum
 
 print(mySum2(20))
+
+def average3(numList):
+    return sum(numList)/len(numList)
+
+print(average3([53,28,54,84,65,60,22,93,62,27,16,25,74,42,4,42,15,96,11,70,83,97,75]))
