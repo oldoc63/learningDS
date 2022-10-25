470b39be2b1ed929c2514336cb22cd175ed050e7
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-24 20:33:30 -0400

    Wandering turtle #437

--- mathAdventures/A-1-Practice/script_3.py
+++ mathAdventures/A-1-Practice/script_3.py
@@ -37,3 +37,17 @@ def gcf(num1, num2):
     return max(gcfList) 
 
 print(gcf(150,138))
+
+# wander.py
+from turtle import *
+from random import randint
+
+speed(0)
+
+def wander():
+    while True:
+        forward(3)
+        if xcor() >= 200 or xcor() <= -200 or ycor() <= -200 or ycor() >= 200:
+            left(randint(90, 180))
+
+wander()
