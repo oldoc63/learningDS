6b39edd69ca9a8f0c2d592d16179910500d1edc9
oldoc63 <leopoldo.olmos@gmail.com>
2022-10-26 09:36:52 -0400

    Finding square roots #437

--- mathAdventures/A-1-Practice/script_3.py
+++ mathAdventures/A-1-Practice/script_3.py
@@ -78,4 +78,24 @@ def numberGame():
             print("Nope. Lower.")
         guess = int(input("What your guess?"))
 
-numberGame()
+#numberGame()
+
+## Finding square roots
+
+def average(a,b):
+    return (a + b) / 2
+
+def squareRoot(num, low, high):
+    '''Finds the square root of num by playing the number guessing Game'''
+    for i in range(20):
+        guess = average(low, high)
+        if guess**2 == num:
+            print(guess)
+            break
+        elif guess**2 > num: #Guess lower
+            high = guess
+        else: #Guess higher
+            low = guess
+    print(guess)
+
+squareRoot(65, 8, 9)
