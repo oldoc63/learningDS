9b9da81c79588fd406d5faca0e5a0d0f29a6e9f2
oldoc63 <leopoldo.olmos@gmail.com>
2022-11-18 13:10:30 -0400

    Nov1820220112

---
+++ NBA Trends/script.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import pearsonr, chi2_contingency
+import seaborn as sns
+
+np.set_printoptions(suppress=True, precision=2)
+
+nba = pd.read_csv('NBA Trends/nba_games.csv')
+
+# print(nba.head)
+
+# Subset Data to 2010 Season, 2014 Season
+nba_2010 = nba[nba.year_id == 2010]
+nba_2014 = nba[nba.year_id == 2014]
+
+# print(nba_2010.head())
+
+# Filter the values in the DataFrame using the team names and selecting only the pts column
+knicks_pts_10 = nba_2010.pts[nba.fran_id=='Knicks']
+nets_pts_10 = nba_2010.pts[nba.fran_id=='Nets']
+
+print(knicks_pts_10.head())
+print(nets_pts_10.head())
