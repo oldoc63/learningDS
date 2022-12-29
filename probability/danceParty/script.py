f771fc9eef81fa1f3a74a706d65ca8987820b2ff
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2022-12-29 12:44:26 -0400

    Use population_distribution() to graph song_tempos #453

--- probability/danceParty/script.py
+++ probability/danceParty/script.py
@@ -4,8 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load the spotify dataset
 spotify_data = pd.read_csv(r'C:\Users\varic\Documents\learningDS\probability\danceParty\spotify_data.csv')
 
+# Preview the dataset
 print(spotify_data.head())
 
+# Select the relevant column
 song_tempos = spotify_data['tempo'] 
+
+# Plot the population distribution with the mean labeled
+population_distribution(song_tempos)
