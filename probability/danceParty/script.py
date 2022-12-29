037b4187cd1f4cd2b119c8fa0741c814a94c7a69
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2022-12-28 21:59:49 -0400

    Loading in data #453

---
+++ probability/danceParty/script.py
@@ -0,0 +1,8 @@
+from helper_functions import choose_statistic, population_distribution, sampling_distribution
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+spotify_data = pd.read_csv('C:\Users\varic\Documents\learningDS\probability\danceParty\spotify_data.csv')
+
