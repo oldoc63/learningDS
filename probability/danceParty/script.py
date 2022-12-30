154822665785aa0232899e9ec491cb474059d032
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2022-12-30 13:06:02 -0400

    Probability of an average tempo of 140 bpm or lower #453

--- probability/danceParty/script.py
+++ probability/danceParty/script.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
 
 # Load the spotify dataset
 spotify_data = pd.read_csv(r'C:\Users\varic\Documents\learningDS\probability\danceParty\spotify_data.csv')
@@ -32,3 +33,7 @@ population_std = np.std(song_tempos)
 
 # Calculate the standard error
 standard_error = population_std/(30**.05)
+
+# Calculate the probability of observing an average tempo of 140 bpm or 
+# lower from a sample of 30 songs
+print(stats.norm.cdf(140, population_mean, standard_error))
