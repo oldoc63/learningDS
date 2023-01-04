105b986cd93b9da1015002012779ae7a937092af
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2023-01-04 09:24:16 -0400

    Calculate and print the p-values for 10 days using a for-loop #456

--- inference/oneSampleTTest2.py
+++ inference/oneSampleTTest2.py
@@ -6,3 +6,8 @@ daily_prices = np.genfromtxt(r'inference\daily_prices.csv', delimiter=',')
 # day 1
 tstat, pval = ttest_1samp(daily_prices[0], 1000)
 print('day 1 p-value:', pval)
+
+# 10 days
+for i in range(10):
+    tstat, pval = ttest_1samp(daily_prices[i], 1000)
+    print('day', [i+1], 'the p-value is:', pval)
