04bbf27f6bd2bbce197d777014a997117f6a10b2
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2023-01-04 09:37:27 -0400

    Change the alternative hypothesis and look how the pval change #456

--- inference/oneSampleTTest2.py
+++ inference/oneSampleTTest2.py
@@ -11,3 +11,9 @@ print('day 1 p-value:', pval)
 for i in range(10):
     tstat, pval = ttest_1samp(daily_prices[i], 1000)
     print('day', [i+1], 'the p-value is:', pval)
+
+# Change the Alternative Hypothesis
+print('\nDay 1 to 10 with a different alternative hypothesis')
+for i in range(10):
+    tstat, pval = ttest_1samp(daily_prices[i], 900)
+    print('day', [i+1], 'the p-value is:', pval)
