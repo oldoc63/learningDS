811d5496a971639fd16d5ee4a3caeeff71639e7d
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2023-01-04 09:18:48 -0400

    Calculate and print out a p-value for day 1 #456

--- inference/oneSampleTTest2.py
+++ inference/oneSampleTTest2.py
@@ -1,5 +1,8 @@
 from scipy.stats import ttest_1samp
 import numpy as np
 
-dayly_prices = np.genfromtxt(r'inference\daily_prices.csv', delimiter=',')
+daily_prices = np.genfromtxt(r'inference\daily_prices.csv', delimiter=',')
 
+# day 1
+tstat, pval = ttest_1samp(daily_prices[0], 1000)
+print('day 1 p-value:', pval)
