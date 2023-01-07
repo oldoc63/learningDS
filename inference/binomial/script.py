d326ad494ee9674ffcb624281decd4c87ba2ac87
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-07 10:51:05 -0400

    Randomly sample 500 visitors who each have a 10% chance of making a purchase #457

--- inference/binomial/script.py
+++ inference/binomial/script.py
@@ -48,3 +48,11 @@ print(min_heads)
 
 max_heads = np.max(outcomes)
 print(max_heads)
+
+# Randomly sample 500 visitors who each have a 10% chance of making a purchase
+null_outcomes = []
+for i in range(10000):
+    simulated_monthly_visitors = np.random.choice(['y', 'n'], size=500, p=[0.1, 0.9])
+    num_purchased = np.sum(simulated_monthly_visitors == 'y')
+    null_outcomes.append(num_purchased)
+print(null_outcomes)
