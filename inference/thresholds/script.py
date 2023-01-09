f269ac3a4b62ba90412d383ff9a05cfc31e2b3c3
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-09 18:58:28 -0400

    Simulate an experiment where the probability of getting a quiz question correct is equal to 70% #459

--- inference/thresholds/script.py
+++ inference/thresholds/script.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.stats import binom_test
+
 # P-Value for first Hypothesis Test
 p_value1 = .062
 # Set the correct value for p_value1_significance
@@ -20,3 +23,16 @@ remove_question_2 = 'yes'
 
 # Set the correct value for outcome
 outcome = 'type two'
+
+#Simulate and experiment where the probability of getting a quiz question correct is equal to 70%
+false_positives = 0
+sig_threshold = 0.05
+
+for i in range(1000):
+    sim_sample = np.random.choice(['correct', 'incorrect'], size=100, p=[0.7, 0.3])
+    num_correct = np.sum(sim_sample == 'correct')
+    p_val = binom_test(num_correct, 100, 0.7)
+    if p_val < sig_threshold:
+        false_positives += 1
+
+print(false_positives/1000)
