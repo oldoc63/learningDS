0483def2e6e260fb4d3145cb434d7ff2e4372afb
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-09 12:05:55 -0400

    Should we remove or leave the question on our site? #459

--- inference/thresholds/script.py
+++ inference/thresholds/script.py
@@ -7,3 +7,13 @@ p_value1_significance = 'not significant'
 p_value2 = 0.013
 # Set the correct value for p_value2_significance
 p_value2_significance = 'significant'
+
+# P-Value for first Hypothesis Test
+p_value1 = .062
+# Set the correct value for remove_question_1
+remove_question_1 = 'no'
+
+# P-Value for second Hypothesis Test
+p_value2 = 0.013
+# Set the correct value for remove_question_2
+remove_question_2 = 'yes'
