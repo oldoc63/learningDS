bfb947f1d3bba7e945627433545c749d54c67ef8
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-10 11:42:02 -0400

    How many test would we have to run at a 0.05 significance level so that the probabilityof at least one type I error would be 50%? #459

--- inference/thresholds/script.py
+++ inference/thresholds/script.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import binom_test
+import matplotlib.pyplot as plt
 
 # P-Value for first Hypothesis Test
 p_value1 = .062
@@ -56,3 +57,22 @@ for i in range(1000):
 
 # Print proportion of type I errors 
 print(false_positives/1000)
+
+# Set a correct value for num_test_50percent
+num_tests_50percent = 15
+
+# Create the plot
+sig_threshold = 0.05
+num_test = np.array(range(50))
+probabilities = 1-((1-sig_threshold)**num_test)
+plt.plot(num_test, probabilities)
+
+# Edit the title and axis labels
+plt.title('Type I Error Rate for Multiple Test', fontsize=15)
+# Label the y-axis
+plt.ylabel('Probability of at Least One Type I Error', fontsize=12)
+# Label the x-axis
+plt.xlabel('Number of Tests', fontsize=12)
+
+# Show the plot
+plt.show()
