ded4ac88247f96bba646b4dd0e1f43d96b48255f
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-11 15:03:03 -0400

    Calculate mean cholesterol levels for patients with heart disease #460

--- inference/heartDisease/script.py
+++ inference/heartDisease/script.py
@@ -8,3 +8,6 @@ no_hd = heart[heart.heart_disease == 'absence']
 
 # get cholesterol levels for patients with heart disease
 chol_hd = yes_hd.chol
+
+# calculate mean cholesterol levels for patients with heart disease
+print(np.mean(chol_hd))
