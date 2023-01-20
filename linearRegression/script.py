3b94d1651f2efa40b995505382b2f7fba2796df3
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2023-01-20 15:58:32 -0400

    Create a scatter plot with hours_studied on the x-axis and score on the y-axis #461

--- linearRegression/script.py
+++ linearRegression/script.py
@@ -3,3 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read in the data
+students = pd.read_csv(r'linearRegression\test_data.csv')
+
+print(students.head())
+
+# Write equation for a line
+y = 9.85 * students.hours_studied + 43
+
+# Create the plot here:
+plt.scatter(students.hours_studied, students.score)
+plt.xlabel('Hours Studied')
+plt.ylabel('Score')
+
+# plt.plot(students.hours_studied, y)
+plt.show()
