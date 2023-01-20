8d0f9fe91904bc4dd5dc5fd7ce80ada51b44dd99
Leopoldo Olmos <leopoldo.olmos@gmail.com>
2023-01-20 17:17:27 -0400

    Edit the equation of the line so that the y-intercept is 45 #466

--- linearRegression/script.py
+++ linearRegression/script.py
@@ -8,7 +8,7 @@ students = pd.read_csv(r'linearRegression\test_data.csv')
 print(students.head())
 
 # Write equation for a line
-predicted_score = 10 * students.hours_studied + 20
+predicted_score = 10 * students.hours_studied + 45
 
 # Create the plot here:
 plt.scatter(students.hours_studied, students.score)
