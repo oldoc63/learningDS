9b07ffa5c1256446169b2b528be6c5edb5f16eae
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-26 22:36:33 -0400

    Adding a friend lunch spending for comparison #476

--- matplotlib/script.py
+++ matplotlib/script.py
@@ -5,3 +5,18 @@ money_spent = [10,12,12,10,14,22,24]
 
 plt.plot(days, money_spent)
 plt.show()
+plt.clf()
+
+# Adding a friend lunch spending for comparison
+# Days of the week:
+days = [0,1,2,3,4,5,6]
+# Your money:
+money_spent = [10,12,12,10,14,22,24]
+# Your Friend's Money:
+money_spent_2 = [11,14,15,15,22,21,12]
+# Plot your money:
+plt.plot(days, money_spent)
+# Plot your friend's money
+plt.plot(days, money_spent_2)
+# Display the result:
+plt.show()
