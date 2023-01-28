2a24027690e03fef7923fe06db09e41169d16ff3
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-28 08:02:20 -0400

    Plot temperature vs months subplot on a figure with 1 row and 2 columns #479

--- matplotlib/script.py
+++ matplotlib/script.py
@@ -142,3 +142,13 @@ plt.title('Second Subplot')
 
 # Display both subplots
 plt.show()
+
+# Flights to Hawaii
+# Define the lists months, temperature and flights_to_hawaii
+months = range(12)
+temperature = [36, 36, 39, 52, 61, 72, 77, 75, 68, 57, 48, 48]
+flights_to_hawaii = [1200, 1300, 1100, 1450, 850, 750, 400, 450, 400, 860, 990, 1000]
+
+plt.subplot(1,2,1)
+plt.plot(months, temperature, color='green')
+plt.show()
