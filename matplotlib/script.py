92e6565bc85f4025723900115bc34c563fe89c94
oldoc63 <leopoldo.olmos@gmail.com>
2023-01-27 22:06:28 -0400

    Keeping track of happiness #478

--- matplotlib/script.py
+++ matplotlib/script.py
@@ -110,3 +110,15 @@ y = [3000, 3005, 3010, 2900, 2950, 3050, 3000, 3100, 2980, 2980, 2920, 3010]
 plt.plot(x, y)
 plt.axis([0,12,2900,3100])
 plt.show()
+plt.clf()
+
+# Labeling axes: Keeping track of happiness
+hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+happiness = [9.8, 9.9, 9.2, 8.6, 8.3, 9.0, 8.7, 9.1, 7.0, 6.4, 6.9, 7.5]
+
+plt.plot(hours, happiness)
+plt.xlabel('Time of day')
+plt.ylabel('Happiness Rating (out of 10)')
+plt.title('My Self-Reported Happiness While Awake')
+plt.show()
+plt.clf()
