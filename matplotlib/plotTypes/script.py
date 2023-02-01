3e5009da5c9feba1ed78013ce2470556978fdc70
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-01 09:47:00 -0400

    Sales at MatplotlibSip #485

--- matplotlib/plotTypes/script.py
+++ matplotlib/plotTypes/script.py
@@ -3,3 +3,15 @@ from matplotlib import pyplot as plt
 # Simple Bar Chart
 heights = [88, 225, 365, 687, 4333, 10756, 30687, 60190, 90553]
 x_values = range(len(heights))
+
+# Number of days in a year on the different planets
+days_in_a_year = [88, 225, 365, 687, 4333, 10756, 30687, 60190, 90553]
+plt.bar(range(len(days_in_a_year)), days_in_a_year)
+plt.show()
+
+# Sales at MatplotSip Cafe
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+sales = [91, 76, 56, 66, 52, 27]
+
+plt.bar(range(len(sales)), sales)
+plt.show()
