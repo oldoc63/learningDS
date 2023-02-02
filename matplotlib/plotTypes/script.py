f5b00ff313cbac353f79b5d494b11cda6785d614
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-02 08:35:57 -0400

    Compare sales at two locations of Matplotsip cafe #485

--- matplotlib/plotTypes/script.py
+++ matplotlib/plotTypes/script.py
@@ -23,3 +23,8 @@ ax.set_xticklabels(drinks)
 plt.bar(range(len(sales)), sales)
 
 plt.show()
+
+# Side by Side Bars: compare sales at two locations of MatplotSip cafe
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+sales1 =  [91, 76, 56, 66, 52, 27]
+sales2 = [65, 82, 36, 68, 38, 40]
