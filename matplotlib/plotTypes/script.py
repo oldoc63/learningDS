7dcaa177faf1c76e6ec33a6c7fc4a59fa03d2c93
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-02 09:33:46 -0400

    Make a list comprehension to generate the x-value set 1 #485

--- matplotlib/plotTypes/script.py
+++ matplotlib/plotTypes/script.py
@@ -28,3 +28,10 @@ plt.show()
 drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
 sales1 =  [91, 76, 56, 66, 52, 27]
 sales2 = [65, 82, 36, 68, 38, 40]
+
+# Make a list comprehension to generate the x-value sets. Start with the first one.
+n = 1  # This is our first dataset (out of 2)
+t = 2 # Number of datasets
+d = len(drinks) # Number of sets of bars
+w = 0.8 # Width of each bar
+store1_x = [t*element + w*n for element in range(d)]
