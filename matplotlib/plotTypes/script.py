2ea65a74e0cf163560fbc2c672427337b091104b
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-03 16:30:33 -0400

    Add a percentage to each slice with one decimal #488

--- matplotlib/plotTypes/script.py
+++ matplotlib/plotTypes/script.py
@@ -151,7 +151,7 @@ plt.show()
 payment_method_names = ["Card Swipe", "Cash", "Apple Pay", "Other"]
 payment_method_freqs = [270, 77, 32, 11]
 
-plt.pie(payment_method_freqs)
+plt.pie(payment_method_freqs, autopct='%0.1f%%')
 plt.legend(payment_method_names)
 plt.show()
 
