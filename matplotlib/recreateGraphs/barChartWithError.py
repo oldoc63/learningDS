5aef64fd108d4dc3c61ba13f6c7ba5f1e7d05e2b
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-05 10:22:15 -0400

    Set the axis to go from -0.5 to 6.5 (x) 70 a 95 (y) #492

--- matplotlib/recreateGraphs/barChartWithError.py
+++ matplotlib/recreateGraphs/barChartWithError.py
@@ -1,10 +1,17 @@
 from matplotlib import pyplot as plt
 
-past_years_average = [82, 84, 83, 86, 74, 84, 90]
+past_years_averages = [82, 84, 83, 86, 74, 84, 90]
 years = [2000, 2001, 2002, 2003, 2004, 2005, 2006]
 error = [1.5, 2.1, 1.2, 3.2, 2.3, 1.7, 2.4]
 
+# Create a figure of width 10 and height 8
 plt.figure(figsize=(10,8))
-plt.bar(range(len(years)), past_years_average, yerr=error, capsize=5)
+
+# Plot the blue bars listed in past_years_averages
+# Add error bars of cap size 5 and heights listed in error
+plt.bar(range(len(years)), past_years_averages, yerr=error, capsize=5)
+
+# Set the axis to go from -0.5 to 6.5 (x) 70 a 95 (y)
+plt.axis([-0.5, 6.5, 70, 95])
 
 plt.show()
