97879b05a2debcaa19e8a726dd4954d8a7ae1e64
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-05 09:25:38 -0400

    Add error bars of capsize 5 and heights from the list error #492

--- matplotlib/recreateGraphs/barChartWithError.py
+++ matplotlib/recreateGraphs/barChartWithError.py
@@ -5,6 +5,6 @@ years = [2000, 2001, 2002, 2003, 2004, 2005, 2006]
 error = [1.5, 2.1, 1.2, 3.2, 2.3, 1.7, 2.4]
 
 plt.figure(figsize=(10,8))
-plt.bar(range(len(years)), past_years_average)
+plt.bar(range(len(years)), past_years_average, yerr=error, capsize=5)
 
 plt.show()
