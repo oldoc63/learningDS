14baa917838a680bab93d28faf64816f7c017dbd
oldoc63 <leopoldo.olmos@gmail.com>
2023-02-07 08:10:34 -0400

    Define the starting heights of the Cs #500

---
+++ matplotlib/recreateGraphs/stackedBars.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+As = [6, 3, 4, 3, 5]
+Bs = [8, 12, 8, 9, 10]
+Cs = [13, 12, 15, 13, 14]
+Ds = [2, 3, 3, 2, 1]
+Fs = [1, 0, 0, 3, 0]
+
+# Bs will go on top of As, but the bottom of Cs
+c_bottom = np.add(As, Bs)
